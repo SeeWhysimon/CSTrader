e9960e870d73d28f694fdebd80c4e04895da33f5
SeeWhysimon <appleforsimondick@icloud.com>
2025-05-23 23:06:01 +0800

    modified:   scripts/main.py

--- scripts/main.py
+++ scripts/main.py
@@ -7,7 +7,7 @@ def collect_data():
     # BUFF 数据采集
     buff_collector = get_collector("buff")
     _, buff_raw_path = buff_collector.collect(
-        config_path="./data_collector/buff/buff_config.json",
+        config_path="../data_collector/buff/buff_config.json",
         save_path="../data/raw/buff"
     )
     buff_collector.clean(
@@ -18,7 +18,7 @@ def collect_data():
     # SteamDT 数据采集
     steamdt_collector = get_collector("steamdt")
     _, steamdt_raw_path = steamdt_collector.collect(
-        config_path="./data_collector/steamdt/steamdt_config.json",
+        config_path="../data_collector/steamdt/steamdt_config.json",
         save_path="../data/raw/steamdt"
     )
     steamdt_collector.append_data(

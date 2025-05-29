131501c0b8b75735bb05b6221810f9f520a7cb04
simon <appleforsimondick@icloud.com>
2025-05-29 15:58:44 +0800

    Constructed a full pipeline.

--- main.py
+++ main.py
@@ -1,40 +1,7 @@
 # scripts/main.py
 
-from scripts.data_processor.plotter import kline_plotter
-from scripts.data_collector import get_collector
-from scripts.data_analyzer.steamdt_analyzer import random_forest_model
-
-def collect_data():
-    # BUFF 数据采集
-    buff_collector = get_collector("buff")
-    buff_collector.run(config_path="./scripts/DataCollector/buff/buff_config.json", 
-                       raw_save_path="./data/raw/buff", 
-                       cleaned_save_path="./data/processed/buff.json")
-
-    # SteamDT 数据采集
-    steamdt_collector = get_collector("steamdt")
-    _, steamdt_raw_path = steamdt_collector.collect(
-        config_path="./scripts/DataCollector/steamdt/steamdt_config.json",
-        save_path="./data/raw/steamdt"
-    )
-    steamdt_collector.append_data(
-        old_path="./data/processed/steamdt.json", 
-        raw_path=steamdt_raw_path
-    )
-
-    kline_plotter("./data/processed/steamdt.json")
+from scripts.pipeline.random_forest_pipeline import RandomForestPipeline
 
 if __name__ == "__main__":
-    collect_data()
-
-    steamdt_analyzer_params = {
-        'max_depth': None, 
-        'max_features': 19, 
-        'min_samples_split': 7, 
-        'n_estimators': 50
-    }
-    
-    analyzer = random_forest_model()
-    analyzer.run(data_path="./data/processed/steamdt.json", 
-                 params=steamdt_analyzer_params, 
-                 steps=14)
+    pipeline = RandomForestPipeline()
+    pipeline.run()

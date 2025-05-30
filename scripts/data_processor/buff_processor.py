861a73a063a3ac7244f67e60ff91e40052be5879
SeeWhysimon <appleforsimondick@icloud.com>
2025-05-30 23:58:54 +0800

    modified:   scripts/data_processor/buff_processor.py modified:   test.py

--- scripts/data_processor/buff_processor.py
+++ scripts/data_processor/buff_processor.py
@@ -16,20 +16,17 @@ class BuffDataProcessor(BaseDataProcessor):
 
         return df
     
-    def clean_data(self, data: pd.DataFrame, save_path: str = None):
+    def clean_data(self, data: pd.DataFrame):
         target_cols = ["asset_info.id", # str
                        "asset_info.info.paintindex", # int
                        "asset_info.info.paintseed", # int
-                       "asset_info.info.paintwear", # str
+                       "asset_info.paintwear", # str
                        "price", # str
                        "transact_time"] # str
         cleaned_data = pd.DataFrame(data[target_cols], columns=target_cols)
         
-        cleaned_data["asset_info.info.paintwear"] = cleaned_data["asset_info.info.paintwear"].astype(float)
+        cleaned_data["asset_info.paintwear"] = cleaned_data["asset_info.paintwear"].astype(float)
         cleaned_data["price"] = cleaned_data["price"].astype(float)
         cleaned_data["transact_time"] = pd.to_datetime(cleaned_data["transact_time"].astype(int), unit='s')
-        
-        if save_path:
-            cleaned_data.to_csv(save_path, index=False)
         
         return cleaned_data
--- test.py
+++ test.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     processor = BuffDataProcessor()
     df = processor.load_data("./data/raw/buff_2025-05-24-15-24-00.json")
 
-    df.to_csv("./data/processed/buff.csv", index=False)
+    cleaned_df = processor.clean_data(df)
+    cleaned_df.to_csv("./data/processed/buff_cleaned.csv", index=False)

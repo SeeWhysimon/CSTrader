50f5cadb2ed98d34cd13b852123ede808dcbd356
SeeWhysimon <appleforsimondick@icloud.com>
2025-07-21 08:44:52 +0800

    modified:   scripts/__init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,15 +5,6 @@ from scripts.data_collector.steamdt.steamdt_collector import SteamDTDataCollecto
 # 后续你可以继续添加其他数据源
 
 def get_collector(source: str):
-    """
-    根据用户指定的数据源名称，返回对应的 DataCollector 实例
-
-    参数:
-        source (str): 数据源标识，例如 "buff", "steamdt"
-
-    返回:
-        BaseDataCollector 的子类实例
-    """
     source = source.lower()
     
     if source == "buff":
